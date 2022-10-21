d6515f33ccbdadcec9e0b9969c8d37532125cf1b
Otabek Kholmirzaev <otabekmirzo@outlook.com>
2022-10-21 16:20:00 +0500

    Add project files.

---
+++ 219. Contains Duplicate II/Program.cs
@@ -0,0 +1,16 @@
+﻿bool result = ContainsNearbyDuplicate(new int[] { 1, 2, 3, 1 }, 3);
+Console.WriteLine(result);
+
+bool ContainsNearbyDuplicate(int[] nums, int k)
+{
+    for (int i = 0; i < nums.Length; i++)
+    {
+        for (int j = i + 1; j <= i + k && j < nums.Length; j++)
+        {
+            if (nums[j] == nums[i])
+                return true;
+        }
+    }
+
+    return false;
+}
